33894fbfd8f91cda47bd40961f43410c0ff90ef2
Connor McElwain <connor@mcelwain.me>
2024-04-14 19:18:50 -0500

    test changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>mcelwain.me</span>,
+  logo: <span>beta.mcelwain.me</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
