ff87c135d0f173924ef0d54becb4da2d4938d238
Connor McElwain <connor@mcelwain.me>
2024-04-14 19:55:48 -0500

    Theme config change

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,6 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>beta.mcelwain.me</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
   chat: {
     link: 'https://discord.com',
   },
