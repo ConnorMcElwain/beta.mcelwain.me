948c3db1dd2b823c0cd3191c0016048335618220
Connor McElwain <connor@mcelwain.me>
2024-04-14 19:59:55 -0500

    GitHub changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>beta.mcelwain.me</span>,
+  project: {
+    link: 'https://github.com/ConnorMcElwain',
+  },
   chat: {
     link: 'https://discord.com',
   },
