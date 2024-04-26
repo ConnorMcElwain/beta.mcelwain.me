12dc2507d112f8b3a33f458c375ce3c04893a6e4
Connor McElwain <connor@mcelwain.me>
2024-04-26 12:14:55 -0500

    footer fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -41,11 +41,6 @@ const config: DocsThemeConfig = {
           McElwain.me
         </a>
       </span>
-    ),
-    text2: (
-      <a href="https://github.com/ConnorMcElwain/beta.mcelwain.me/commit/main" target="_blank">
-      Change Logs
-    </a>
     )
   },
 }
