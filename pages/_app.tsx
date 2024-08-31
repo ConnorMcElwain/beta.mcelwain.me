652e2de28ad20752ce181c8ba7500dad65b541de
Connor McElwain <connor@mcelwain.me>
2024-08-31 11:00:58 -0500

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // These styles apply to every route in the application
-import '@/styles/globals.css'
+import '@/components/global.css'
 import type { AppProps } from 'next/app'
  
 export default function App({ Component, pageProps }: AppProps) {
