a13cb0a6c90c05d22fdf40d2caf8d5dafcd55401
Connor McElwain <connor@mcelwain.me>
2024-08-31 13:35:10 -0500

    test

---
+++ components/TestComponent.tsx
@@ -0,0 +1,8 @@
+// /components/TestComponent.tsx
+export default function TestComponent() {
+    return (
+      <div className="bg-blue-500 text-white p-4">
+        Tailwind CSS is working if this box is blue with white text!
+      </div>
+    );
+  }
