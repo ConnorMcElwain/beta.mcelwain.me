fe85a983bfe9eebfb29c338fc2015f854c490189
Connor McElwain <connor@mcelwain.me>
2024-08-31 14:24:26 -0500

    Removed test component (tailwindcss)

--- components/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-// /components/TestComponent.tsx
-export default function TestComponent() {
-    return (
-      <div className="bg-blue-500 text-white p-4">
-        Tailwind CSS is working if this box is blue with white text!
-      </div>
-    );
-  }
