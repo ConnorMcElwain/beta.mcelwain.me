417643de9b9038de37292040c2c82a4042b9d69d
Connor McElwain <connor@mcelwain.me>
2024-10-23 16:45:37 -0500

    Test Contact Form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 // These styles apply to every route in the application
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import { FormspreeProvider } from '@formspree/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <FormspreeProvider project={process.env.NEXT_PUBLIC_FORMSPREE_PROJECT}>
+      <Component {...pageProps} />
+    </FormspreeProvider>
+  );
+}
+
+export default MyApp;
