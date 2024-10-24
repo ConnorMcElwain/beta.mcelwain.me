92467f804760ab31fc378f2fcb964607c9875fe3
Connor McElwain <connor@mcelwain.me>
2024-10-23 22:36:04 -0500

    Form reverts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 // These styles apply to every route in the application
 import '../styles/global.css';
-import { FormspreeProvider } from '@formspree/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <FormspreeProvider project={process.env.NEXT_PUBLIC_FORMSPREE_PROJECT}>
-      <Component {...pageProps} />
-    </FormspreeProvider>
-  );
+  return <Component {...pageProps} />;
 }
